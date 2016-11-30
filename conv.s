042d33d8969195f92860184be2a445fec1d73411
tmprender <tmprender@gmail.com>
2016-11-30 13:50:19 -0500

    sed worked

--- conv.s
+++ conv.s
@@ -29,185 +29,185 @@ inner:
 #	add $t0, $t0, $a0
 
 
-	lb $t0 ($t0)	# input R val pix 0
+	lb $t0 ($s7)	# input R val pix 0
 	lb $t1 0($a1) 	# kernel 0
 	mult $t0 $t1
 	mflo $t3      	
-	addi $t0 1	# increment: G value, same pixel
+	addi $s7 1	# increment: G value, same pixel
 
-	lb $t0 ($t0) 	# input G val pix 0
+	lb $t0 ($s7) 	# input G val pix 0
 	mult $t0 $t1 	
 	mflo $t5	
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input B val pix 0
+	lb $t0 ($s7)	# input B val pix 0
 	mult $t0 $t1
 	mflo $t7
-	addi $t0 1	#increment: next pixel
+	addi $s7 1	#increment: next pixel
 
 ####
 	
-	lb $t0 ($t0) 	# input R val pix 1 
+	lb $t0 ($s7) 	# input R val pix 1 
 	lb $t1 1($a1) 	# kernel 1
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 1
+	lb $t0 ($s7)	# input G val pix 1
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 1
+	lb $t0 ($s7) 	# input B val pix 1
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 2 
+	lb $t0 ($s7) 	# input R val pix 2 
 	lb $t1 2($a1) 	# kernel 2
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 2
+	lb $t0 ($s7)	# input G val pix 2
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 2
+	lb $t0 ($s7) 	# input B val pix 2
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 3 
+	lb $t0 ($s7) 	# input R val pix 3 
 	lb $t1 3($a1) 	# kernel 3
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 3
+	lb $t0 ($s7)	# input G val pix 3
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 3
+	lb $t0 ($s7) 	# input B val pix 3
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 4 
+	lb $t0 ($s7) 	# input R val pix 4 
 	lb $t1 4($a1) 	# kernel 4
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 4
+	lb $t0 ($s7)	# input G val pix 4
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 4
+	lb $t0 ($s7) 	# input B val pix 4
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 5 
+	lb $t0 ($s7) 	# input R val pix 5 
 	lb $t1 5($a1) 	# kernel 5
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 5
+	lb $t0 ($s7)	# input G val pix 5
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 5
+	lb $t0 ($s7) 	# input B val pix 5
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 6 
+	lb $t0 ($s7) 	# input R val pix 6 
 	lb $t1 6($a1) 	# kernel 6
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 6
+	lb $t0 ($s7)	# input G val pix 6
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 6
+	lb $t0 ($s7) 	# input B val pix 6
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 7
+	lb $t0 ($s7) 	# input R val pix 7
 	lb $t1 7($a1) 	# kernel 7
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 7
+	lb $t0 ($s7)	# input G val pix 7
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 7
+	lb $t0 ($s7) 	# input B val pix 7
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
-	addi $t0 1      # increment: next pixel
+	addi $s7 1      # increment: next pixel
 
 ####
 
-	lb $t0 ($t0) 	# input R val pix 8 
+	lb $t0 ($s7) 	# input R val pix 8 
 	lb $t1 8($a1) 	# kernel 8
 	mult $t0 $t1
 	mflo $t4      	
 	add $t3 $t3 $t4 # sum RED
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0)	# input G val pix 8
+	lb $t0 ($s7)	# input G val pix 8
 	mult $t0 $t1
 	mflo $t6
 	add $t5 $t5 $t6 # sum GREEN
-	addi $t0 1	
+	addi $s7 1	
 
-	lb $t0 ($t0) 	# input B val pix 8
+	lb $t0 ($s7) 	# input B val pix 8
 	mult $t0 $t1
 	mflo $t8
 	add $t7 $t7 $t8	# sum blue
@@ -343,7 +343,7 @@ print_ppm:
 	sw   $ra, 0($sp)
 	
 	move $t0, $a0          # t0 = pointer to image
-	lw   $t1, 0($t0)       # t1 = N 
+	lw   $t1, 0($s7)       # t1 = N 
 
 	li $a0, 80    # print P
 	li $v0, 11
@@ -385,19 +385,19 @@ print_ppm__start_row:
 print_ppm__start_pixel:	
 	beqz $t3, print_ppm__row_done   # check if row done
 	
-	lbu $a0, 0($t0) # print R
+	lbu $a0, 0($s7) # print R
 	li $v0, 1
 	syscall
 	li $a0, 32     # print space
 	li $v0, 11
 	syscall
-	lbu $a0, 1($t0) # print G
+	lbu $a0, 1($s7) # print G
 	li $v0, 1
 	syscall
 	li $a0, 32     # print space
 	li $v0, 11
 	syscall
-	lbu $a0, 2($t0) # print B
+	lbu $a0, 2($s7) # print B
 	li $v0, 1
 	syscall
 	li $a0, 32     # print space
